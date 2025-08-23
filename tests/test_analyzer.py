00c605e3258942275df504e1c75fbfdcbd6ec5b0
Anand Singh <111451218+Michaelanand123singh@users.noreply.github.com>
2025-08-23 05:49:45 +0530

    eijr

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="127.0.0.1",
-        port=8000,
+        port=8080,
         reload=settings.DEBUG,
         log_level="info" if not settings.DEBUG else "debug"
     )
